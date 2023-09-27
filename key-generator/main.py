1221bf63dfda2351ecce8c2582d36da91dfddf0f
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-09-28 00:39:54 +0300

    added post req

--- key-generator/main.py
+++ key-generator/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from requests import post
 from keygen_script import generate_rsa_keys
 
 app = FastAPI()
@@ -7,5 +7,14 @@ app = FastAPI()
 
 @app.post("/key-generator")
 def generate_keys(body: dict[str, str]):
+    # generating rsa keys
     body["private_key"], body["public_key"] = generate_rsa_keys()
+
+    # creating body without private key for elector commission service
+    post_body = {i: body[i] for i in body if i != 'private_key'}
+
+    # sending post request to elector commission service
+    post("http://0.0.0.0:8000/elector-commission/", post_body)
+
+    # returning private and public key attached to passport id back to election portal
     return body
