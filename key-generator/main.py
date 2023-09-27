129cdcb1ad7286542068205205e575d6272cba7e
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-09-28 00:48:20 +0300

    optimised

--- key-generator/main.py
+++ key-generator/main.py
@@ -10,11 +10,8 @@ def generate_keys(body: dict[str, str]):
     # generating rsa keys
     body["private_key"], body["public_key"] = generate_rsa_keys()
 
-    # creating body without private key for elector commission service
-    post_body = {i: body[i] for i in body if i != 'private_key'}
-
-    # sending post request to elector commission service
-    post("http://0.0.0.0:8000/elector-commission/", post_body)
+    # sending post request to elector commission service without private key
+    post("http://0.0.0.0:8000/elector-commission/", {i: body[i] for i in body if i != 'private_key'})
 
     # returning private and public key attached to passport id back to election portal
     return body
